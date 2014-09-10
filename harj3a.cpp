c3140211026e77fcac42c7a1f9a2993668cb73fa
Tapsa93 <Tapio.pek@hotmail.com>
2014-09-10 22:41:48 +0300

    Harj3a
    Harjoitus 3

---
+++ harj3a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "***************************";
+	cout << endl;
+	cout << "* *";
+	cout << endl;
+	cout << "*Tapio*";	
+	cout << endl;
+	cout << "*Pekkarinen*";
+	cout << endl;
+	cout << "*_________________________*";
+	cout << endl;
+	cout << "* *";
+	cout << endl;
+	cout << "*Kynnystie 1f B33*";
+	cout << endl;
+	cout << "*40640 JKL*";
+	cout << endl;
+	cout << "* *";
+	cin.get();
+	return 0;
+}
